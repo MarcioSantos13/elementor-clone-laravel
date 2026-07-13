9058be7f140f756b298e77ebff19c0cf53730e4a
Marcio <38866057+MarcioSantos13@users.noreply.github.com>
2026-07-13 15:11:12 -0300

    Update app.blade.php

--- resources/views/page-builder/layouts/app.blade.php
+++ resources/views/page-builder/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield('title', 'Page Builder') - {{ config('app.name') }}</title>
+    <title>@yield('title', 'Page Builder') - CEAD</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
     <style>
